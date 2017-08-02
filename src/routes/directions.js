be05d25f7b2c2e32824a93e1bd244790be0ef767
Stephen Anderson <stephenanderson@Stephens-MacBook-Air.local>
2017-08-02 14:09:18 -0700

    Add '/alerts' route to 511.org to fetch alerts

--- src/routes/directions.js
+++ src/routes/directions.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fetch = require('node-fetch');
 
 const router = express.Router();
 require('dotenv').config();
@@ -20,7 +21,7 @@ router.get('/directions', (req, res) => {
     origin: request.origin,
     destination: request.destination,
     mode: request.mode || 'transit',
-    alternatives: request.alternatives || false,
+    alternatives: request.alternatives === 'true' || false,
   };
 
   if (request.arrival_time === undefined) {
@@ -35,15 +36,47 @@ router.get('/directions', (req, res) => {
     apiQuery.transit_mode = request.transit_mode;
   }
 
+  const currentTimeInSeconds = Math.round(Date.now() / 1000);
+
   return googleMapsClient.directions(apiQuery).asPromise()
     .then((response) => {
       if (response.json.status !== 'OK') {
         return res.sendStatus(404);
       }
 
-      return res.send(response.json.routes);
+      if (apiQuery.alternatives) {
+        response.json.routes.sort((a, b) => (
+          ((a.legs[0].arrival_time.value - currentTimeInSeconds) + a.legs[0].departure_time.value) -
+          ((b.legs[0].arrival_time.value - currentTimeInSeconds) + b.legs[0].departure_time.value)
+        ));
+      }
+
+      return res.send(response.json.routes.slice(0, 2));
     })
     .catch(err => err);
+});
+
+router.get('/alerts', (req, res) => {
+  const key = process.env.FIVE_ONE_ONE_API_KEY
+     fetch(`http://api.511.org/transit/servicealerts?api_key=${key}&format=json`)
+     .then(response => {
+       return response.text();
+    })
+    .then(json => {
+      let data = JSON.parse(json.slice(1))
+      let alerts = {};
+      let id = 1;
+      data._entity.forEach(alert => {
+                alerts[id] = { subject: alert._alert._header_text._translation[0]._text,
+                description: alert._alert._description_text._translation[0]._text
+              }
+              id++
+            })
+			res.send(alerts);
+		})
+    .catch(err => {
+      console.error(err)
+    })
 });
 
 module.exports = router;
