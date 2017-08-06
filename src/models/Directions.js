3b7b445db3986b5c475126b5aa5b0b511bdf5be6
Kurtis Houser <houserkurtis@gmail.com>
2017-08-06 09:31:35 -0700

    Filter out unneeded routes to resolve sort bug

--- src/models/Directions.js
+++ src/models/Directions.js
@@ -7,30 +7,33 @@ const googleMapsClient = require('@google/maps').createClient({
 
 class Directions {
   getDirections(apiQuery) {
-    console.log('----- apiQuery -----');
-    console.log(apiQuery);
-    const currentTimeInSeconds = Math.round(Date.now() / 1000);
+    const currentUtcTimeInSeconds = Math.round(Date.now() / 1000);
 
     return googleMapsClient.directions(apiQuery).asPromise()
       .then((response) => {
         if (response.json.status !== 'OK') {
-          console.log(response);
-          return res.sendStatus(404);
+          return response.json;
+          // return res.sendStatus(404);
         }
 
-        if (apiQuery.alternatives) {
+        // sometimes Google Directions returns routes without
+        // arrival_time and departure_time so we exclude them
+        response.json.routes = response.json.routes.filter(route =>
+          route.legs[0].arrival_time && route.legs[0].departure_time);
+
+        // sort multiple routes by best option
+        if (response.json.routes.length > 1) {
           response.json.routes.sort((a, b) => (
-            ((a.legs[0].arrival_time.value - currentTimeInSeconds) + a.legs[0].departure_time.value) -
-            ((b.legs[0].arrival_time.value - currentTimeInSeconds) + b.legs[0].departure_time.value)
+            ((a.legs[0].arrival_time.value - currentUtcTimeInSeconds)
+              + a.legs[0].departure_time.value) -
+            ((b.legs[0].arrival_time.value - currentUtcTimeInSeconds)
+              + b.legs[0].departure_time.value)
           ));
         }
 
         return response.json.routes.slice(0, 2);
       })
-      .catch((err) => {
-        console.log(err);
-        return err;
-      });
+      .catch(err => err);
   }
 }
 
